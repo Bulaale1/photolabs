c17ae4f39fa57dd3d48bdd04fd1b538846c21a52
Yasin Hussein <bulaale116@gmail.com>
2024-02-23 10:26:23 -0700

    add an alt attribute to the user profile image for better accessibility

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -14,7 +14,7 @@ const PhotoListItem = (props) => {
       <div className="photo-list__user-details">
 
       <div className="photo-list__user-info">
-      <img src={profile} className="photo-list__user-profile "/>
+      <img src={profile} className="photo-list__user-profile " alt={`${username}'s profile picture`}/>
       <p>{username}</p>
       <p className="photo-list__user-location ">
         {location.city}, {location.country}
