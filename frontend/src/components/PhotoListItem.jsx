122c394f8059b72ecf7927b2c8900b67bf5ebeda
Yasin Hussein <bulaale116@gmail.com>
2024-02-23 10:08:14 -0700

    photoitemlist component and style are completed

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,42 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
+const PhotoListItem = (props) => {
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
-  /* Insert React */
+  const { id, location, imageSource, username, profile } = props.data;
+
+  // return (
+  //   <div className="photo-list__item">
+
+  //   <img src={imageSource} alt={`Photo ${id}`} />
+
+  //   <p>{`${username}`}</p>
+
+  //   <img src={profile} alt={`${username}'s profile`} />
+
+  //   <p>{location.city},{location.country}</p>
+      
+  //   </div>
+  // );
+  return (
+    <div className="photo-list__item">
+      <img src={imageSource} alt={`Photo ${id}`} className="photo-list__image"/>
+      
+      <div className="photo-list__user-details">
+
+      <div className="photo-list__user-info">
+        <p>{username}</p>
+        <img src={profile}    className="photo-list__user-profile "/>
+      </div>
+       
+      </div>
+      <p className="photo-list__user-location ">
+      {location.city}, {location.country}
+      </p>
+      
+    </div>
+  );
 };
 
 export default PhotoListItem;
