3b54b2d9f5f5d17e57efdabc07138fc6ad652467
Yasin Hussein <bulaale116@gmail.com>
2024-02-25 08:14:06 -0700

    TopNavigationbar is implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,14 @@ import PhotoList from 'components/PhotoList';
 
 import './App.scss';
 import TopicList from 'components/TopicList';
+import TopNavigation from 'components/TopNavigationBar';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
   return (
     <div className="App">
+    <TopNavigation />
     <TopicList />
      <PhotoList />
     </div>
--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -5,7 +5,14 @@ import '../styles/TopNavigationBar.scss'
 const TopNavigation = () => {
   return (
     <div className="top-nav-bar">
-      <span className="top-nav-bar__logo">PhotoLabs</span>
+      <span className="top-nav-bar__logo">PhotoLabs
+      <div className="nav-links">
+        <a href="#">Topics</a>
+        <a href="#">Liked Photos</a>
+        <img src="#" alt="App Logo" />
+        
+      </div>
+      </span>
     </div>
   )
 }
