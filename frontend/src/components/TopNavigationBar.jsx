c2eb5cd3cbc6599028e8b16382ee5ad93818af27
Yasin Hussein <bulaale116@gmail.com>
2024-02-25 08:20:20 -0700

    add TopicList and favBadge to the TopNavigationbar

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -5,14 +5,9 @@ import '../styles/TopNavigationBar.scss'
 const TopNavigation = () => {
   return (
     <div className="top-nav-bar">
-      <span className="top-nav-bar__logo">PhotoLabs
-      <div className="nav-links">
-        <a href="#">Topics</a>
-        <a href="#">Liked Photos</a>
-        <img src="#" alt="App Logo" />
-        
-      </div>
-      </span>
+      <span className="top-nav-bar__logo">PhotoLabs </span>
+      <TopicList />
+      <FavBadge />
     </div>
   )
 }
