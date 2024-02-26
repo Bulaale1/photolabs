ee5f86971eaaad571e541e084eddf134a37d6227
Yasin Hussein <bulaale116@gmail.com>
2024-02-26 15:39:46 -0700

    close button is added and photomodalDetails component is cleaned

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,23 +1,15 @@
 import React, { useState } from 'react';
 import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
-
 const PhotoDetailsModal = (props) => {
-  const [modalOpen, setModalOpen] = useState(false);
   const {closeModal} = props;
-
-  const openModal = () => {
-    setModalOpen(true);
-  };
   return (
-    <div>
-      <button onClick={openModal}>Open Modal</button>
-      <div className={`photo-details-modal ${modalOpen ? 'open' : ''}`}>
+      <div className="photo-details-modal">
         <button className="photo-details-modal__close-button" onClick={closeModal}>
           <img src={closeSymbol} alt="close symbol" />
         </button>
       </div>
-    </div>
+    
   );
 };
 
