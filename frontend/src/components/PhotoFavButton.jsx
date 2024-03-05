62c3f44913635e70466bb6d5ad652e95ac5f8904
Yasin Hussein <bulaale116@gmail.com>
2024-03-05 11:31:42 -0700

    photofavbutton component is cleaned

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,33 +1,13 @@
 import React, { useCallback, useState } from 'react';
 import '../styles/PhotoFavButton.scss';
 import FavIcon from './FavIcon';
-// function PhotoFavButton(props) {
-
-//   return (
-//     <div className="photo-list__fav-icon" onClick={props.toggleFavourite}>
-//       <div className="photo-list__fav-icon-svg">
-//         <FavIcon selected={props.isFavourited}/>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default PhotoFavButton;
-
 function PhotoFavButton(props) {
-
-  const [isClicked, setIsClicked] = useState(false);
-  
-  const handleIconClick = () => {
-    
-    setIsClicked((prev) => !prev);
-  };
 
   return (
     <div>
-    <div className={`photo-list__fav-icon ${isClicked ? 'photo-list__fav-icon-svg' : ''}`}>
-      <div className="photo-list__fav-icon-svg" onClick={handleIconClick}>
-      <FavIcon isClicked={isClicked} />
+    <div className="photo-list__fav-icon" onClick={props.toggleFavourite} >
+      <div className="photo-list__fav-icon-svg">
+      <FavIcon selectedImage={props.isFavourited} />
         
       </div>
     </div>
