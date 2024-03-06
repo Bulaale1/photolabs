c406fe77c8dd1bf960db3038508888b8bfc1c345
Yasin Hussein <bulaale116@gmail.com>
2024-03-06 11:22:59 -0700

    made some change to Display a Notification for Favourited Photo

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -6,6 +6,8 @@ import FavBadge from './FavBadge';
 const TopNavigation = (props) => {
   const {topics, FavoritePhotos}= props
   // const {topics,favPhotos}= props.topics;
+  //set selectedImage initially  to false
+  const selectedImage = false;
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs </span>
