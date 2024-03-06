2ca9b523f0c192d2bef5f88ad6d7fc5019858f77
Yasin Hussein <bulaale116@gmail.com>
2024-03-06 10:59:38 -0700

    Display a Notification for Favourited Photos re-arranged

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -4,12 +4,13 @@ import '../styles/TopNavigationBar.scss'
 import TopicList from './TopicList';
 import FavBadge from './FavBadge';
 const TopNavigation = (props) => {
-  const {topics,favPhotos}= props.topics;
+  const {topics, FavoritePhotos}= props
+  // const {topics,favPhotos}= props.topics;
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs </span>
       <TopicList topics = {topics} />
-      <FavBadge isFavPhotoExist={favPhotos.length>0} selectedImage={selectedImage}/>
+      <FavBadge isFavPhotoExist={FavoritePhotos.length>0} selectedImage={selectedImage}/>
     </div>
   )
 }
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -4,14 +4,12 @@ import TopNavigation from '../components/TopNavigationBar'
 import PhotoList from '../components/PhotoList';
 const HomeRoute = (props) => {
     const { setDisplayModal,FavoritePhotos,toggleFavourite, favourites} = props; 
+    //FavoritePhotos={FavoritePhotos}
   return (
     <div className="home-route">
-      <TopNavigation 
-      topics={props.topics} 
-      FavoritePhotos={FavoritePhotos}/>
+    <TopNavigation topics={props.topics} FavoritePhotos={favourites}/>
       <PhotoList photos={props.photos} 
       setDisplayModal={setDisplayModal}
-      FavoritePhotos={FavoritePhotos}
       toggleFavourite={toggleFavourite}
       favourites={favourites}/>
     </div>
