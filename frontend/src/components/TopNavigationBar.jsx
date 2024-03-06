279f30647dcb3595c368f0c9b67c1a6c26aef532
Yasin Hussein <bulaale116@gmail.com>
2024-03-06 11:35:24 -0700

    changed favourites to  favouritePhotosExist in homerout and topnavigation components  to keep the prop names consistent

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -4,14 +4,14 @@ import '../styles/TopNavigationBar.scss'
 import TopicList from './TopicList';
 import FavBadge from './FavBadge';
 const TopNavigation = (props) => {
-  const {topics, FavoritePhotos}= props
+  const {topics, favouritePhotosExist}= props
   // const {topics,favPhotos}= props.topics;
   //set selectedImage initially  to false
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs </span>
       <TopicList topics = {topics} />
-      <FavBadge isFavPhotoExist={FavoritePhotos.length>0} />
+      <FavBadge isFavPhotoExist={favouritePhotosExist} />
     </div>
   )
 }
