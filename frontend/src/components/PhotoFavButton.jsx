c33ea17794f1ec3ead5ae40a2fd2cb397f7aedbc
Yasin Hussein <bulaale116@gmail.com>
2024-03-06 16:58:04 -0700

    made some cleaning to PhotoFavButton and photoListItem

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,25 +1,25 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import FavIcon from './FavIcon';
 
 function PhotoFavButton(props) {
-  const { photoId, toggleFavourite, favourites=[] } = props;
-  const [isClicked, setIsClicked] = useState(false);
-    // Check if the photo is already favorited
-    const isAlreadyFavorited = favourites.includes(photoId);
+ const { photoId, toggleFavourite, favourites = [] } = props;
+ // Check if the photo is already favorited
+ const isAlreadyFavorited = favourites.includes(photoId);
 
-  const handleToggleFavourite = () => {
+ const handleToggleFavourite = () => {
     toggleFavourite(photoId);
-    setIsClicked(!isClicked);
-  };
-   // Dynamically set the class name based on the isClicked state
- const className = isClicked ? "photo-list__fav-icon-svg" : "photo-list__fav-icon";
-  return (
+ };
+
+ // Dynamically set the class name based on the isAlreadyFavorited variable
+ const className = isAlreadyFavorited ? "photo-list__fav-icon-svg" : "photo-list__fav-icon";
+
+ return (
     <div>
       <div className={className} onClick={handleToggleFavourite}>
-          <FavIcon isFavourite={isAlreadyFavorited} />
+        <FavIcon isFavourite={isAlreadyFavorited} />
       </div>
     </div>
-  );
+ );
 }
 
 export default PhotoFavButton;
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,45 +1,42 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
 import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
-  const { id, location,urls, user} = props.photo;
-  const { setDisplayModal,onImageClick,toggleFavourite, favourites=[]  } = props;
-  const isFavourite = favourites.includes(id);
-  const handleImageClick = () => {
+ const { id, location, urls, user } = props.photo;
+ const { setDisplayModal, toggleFavourite, favourites = [] } = props;
+
+ const handleImageClick = () => {
     // Your existing logic for handling image click
     if (props.onImageClick) {
       props.onImageClick({ id, location, urls, user });
     }
  };
-  return (
+
+ return (
     <div className="photo-list__item">
-    <PhotoFavButton
+      <PhotoFavButton
         toggleFavourite={toggleFavourite}
-        isFavourite={isFavourite}
         photoId={id}
+        favourites={favourites} // Pass the favorites prop to PhotoFavButton
       />
-    <img src={urls.full} alt={`Photo ${id}`} className="photo-list__image"
+      <img src={urls.full} alt={`Photo ${id}`} className="photo-list__image"
         onClick={() => {
-        setDisplayModal(true);
-        handleImageClick(); 
-      } }
-    />
+          setDisplayModal(true);
+          handleImageClick();
+        }}
+      />
       <div className="photo-list__user-details">
-      
-      <div className="photo-list__user-info">
-      
-      <img src={user.profile} className="photo-list__user-profile " alt={`${user.profile}'s profile picture`}/>
-      <p>{user.name}</p>
-      <p className="photo-list__user-location ">
-        {location.city}, {location.country}
-      </p>
+        <div className="photo-list__user-info">
+          <img src={user.profile} className="photo-list__user-profile" alt={`${user.profile}'s profile picture`} />
+          <p>{user.name}</p>
+          <p className="photo-list__user-location">
+            {location.city}, {location.country}
+          </p>
+        </div>
       </div>
-      </div>
-      
     </div>
-  );
+ );
 };
 
 export default PhotoListItem;
