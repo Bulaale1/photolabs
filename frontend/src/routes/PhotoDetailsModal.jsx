b05faf8d80de6187870fa8d5e5110bc53fbaad66
Yasin Hussein <bulaale116@gmail.com>
2024-03-06 19:15:48 -0700

    passed toggleFavourite function and favourites state as props to favIcon

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -5,14 +5,16 @@ import closeSymbol from '../assets/closeSymbol.svg';
 // import photos from 'mocks/photos';
 import FavIcon from 'components/FavIcon';
 const PhotoDetailsModal = (props) => {
-  const {closeModal,selectedImage} = props;
+  const {closeModal,selectedImage,toggleFavourite, favourites} = props;
   console.log(selectedImage)
   return (
       <div className="photo-details-modal">
         <button className="photo-details-modal__close-button" onClick={closeModal}>
           <img src={closeSymbol} alt="close symbol" />
         </button>
-        <FavIcon selected={selectedImage} />
+        <FavIcon selected={selectedImage} 
+        toggleFavourite={toggleFavourite}
+        favourites={favourites}/>
       </div>
   );
 };
