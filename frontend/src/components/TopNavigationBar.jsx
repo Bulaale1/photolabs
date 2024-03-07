b63a46b29c23dd1bf6ace25011b2e14a8e2be2ef
Yasin Hussein <bulaale116@gmail.com>
2024-03-06 19:43:44 -0700

    pass the favorites state to the TopNavigation component and use it to conditionally render the heart icon.

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -4,12 +4,13 @@ import '../styles/TopNavigationBar.scss'
 import TopicList from './TopicList';
 import FavBadge from './FavBadge';
 const TopNavigation = (props) => {
-  const {topics, favouritePhotosExist}= props
+  const {topics, favorites = [] }= props
+  favoritePhotosExist = favorites.length > 0;
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs </span>
       <TopicList topics = {topics} />
-      <FavBadge isFavPhotoExist={favouritePhotosExist} />
+      {favoritePhotosExist && <FavBadge isFavPhotoExist={favouritePhotosExist} />}
     </div>
   )
 }
